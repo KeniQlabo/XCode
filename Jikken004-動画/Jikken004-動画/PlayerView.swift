5a7d9bf042b806f8462da082ef949891611882bb
kobikane <kobikane@qlabos.co.jp>
2021-01-10 11:21:49 +0900

    test

---
+++ Jikken004-動画/Jikken004-動画/PlayerView.swift
@@ -0,0 +1,22 @@
+import UIKit
+import AVKit
+
+class PlayerView: UIView {
+    var player: AVPlayer? {
+        get {
+            return playerLayer.player
+        }
+        set {
+            playerLayer.player = newValue
+        }
+    }
+    
+    var playerLayer: AVPlayerLayer {
+        return layer as! AVPlayerLayer
+    }
+    
+    // Override UIView property
+    override static var layerClass: AnyClass {
+        return AVPlayerLayer.self
+    }
+}
